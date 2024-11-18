86637b9c3db8e7cf48b4709f081f9d1acc4349a9
BigJT <johntrihy@gmail.com>
2024-11-18 11:57:55 +0000

    Update backend.tf

--- Subscriptions/jt-training-subscription/rg-connectivity-001/backend.tf
+++ Subscriptions/jt-training-subscription/rg-connectivity-001/backend.tf
@@ -5,9 +5,9 @@ terraform {
         version = "=3.0.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name     = "rg-terraform-state-001"
-    storage_account_name    = "cloudninjaterraformstate"
+    backend "azurerm" {
+    resource_group_name     = "tfstate-uksouth-rg"
+    storage_account_name    = "terraformbackendjtr01"
     container_name          = "tfstate"
     key                     = "GitHub-Terraform-rg-connectivity-001"
   }
